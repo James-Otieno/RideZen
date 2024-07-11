91d2b22542670dd712e91979a983e7d96fb00f32
james-otieno <jemo9434@gmail.com>
2024-07-11 23:19:53 +0300

    updated DbContext

---
+++ RideZen.Domain/Entities/RideZenContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideZen.Domain.Entities
+{
+    public class RideZenContext:DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Ride> Rides { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public RideZenContext(DbContextOptions<RideZenContext>opt):base(opt)
+        {
+
+        }
+    }
+}
